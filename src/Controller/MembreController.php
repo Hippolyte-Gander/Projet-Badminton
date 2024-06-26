43f5c2d36d69c81dede5ed0a1ca24a31fbb94502
Hippolyte G <hippolyte.gander5@gmail.com>
2024-06-26 11:43:05 +0200

    ajout liste membre et détail membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Membre;
+use App\Repository\MembreRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MembreController extends AbstractController
 {
     #[Route('/membre', name: 'app_membre')]
-    public function index(): Response
+    public function index(MembreRepository $membreRepository): Response
     {
+        $membres = $membreRepository->findBy([], ["nom"=> "ASC"]);
         return $this->render('membre/index.html.twig', [
-            'controller_name' => 'MembreController',
+            'membres' => $membres
+        ]);
+    }
+
+    #[Route('/membre/{id}', name: 'show_membre')]
+    public function show(Membre $membre): Response
+    {
+        return $this->render('membre/show.html.twig', [
+            'membre' => $membre
         ]);
     }
 }
