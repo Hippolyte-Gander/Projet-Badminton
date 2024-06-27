b93f80b9fd07f6e347f3c3d96dadfea465663cec
Hippolyte G <hippolyte.gander5@gmail.com>
2024-06-27 12:12:11 +0200

    modif mise en page et controlleur membre / événement

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Evenement;
+use App\Repository\EvenementRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EvenementController extends AbstractController
 {
     #[Route('/evenement', name: 'app_evenement')]
-    public function index(): Response
+    public function index(EvenementRepository $evenementRepository): Response
     {
+        $evenements = $evenementRepository->findBy([], ["dateDebut"=> "ASC"]);
         return $this->render('evenement/index.html.twig', [
-            'controller_name' => 'EvenementController',
+            'evenements' => $evenements
+        ]);
+    }
+
+    #[Route('/evenement/{id}', name: 'show_evenement')]
+    public function show(Evenement $evenement): Response
+    {
+        return $this->render('evenement/show.html.twig', [
+            'evenement' => $evenement
         ]);
     }
 }
