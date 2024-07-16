06d811ae89494a97be4b7f46a101b93fb47dfd31
Hippolyte G <hippolyte.gander5@gmail.com>
2024-07-16 15:14:47 +0200

    tests fix bug affichage détail membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -23,11 +23,26 @@ class MembreController extends AbstractController
 
     // ------------- AFFICHER DÉTAIL MEMBRE ------------- ne fonctionne pas
 
+    // #[Route('/membre/{id}', name: 'show_membre')]
+    // public function show(Membre $membre): Response
+    // {
+    //     return $this->render('membre/show.html.twig', [
+    //         'membre' => $membre
+    //     ]);
+    // }
+
     #[Route('/membre/{id}', name: 'show_membre')]
-    public function show(Membre $membre): Response
+    public function show(int $id, MembreRepository $membreRepository): Response
     {
+        $membre = $membreRepository->find($id);
+        
+        if (!$membre) {
+            throw $this->createNotFoundException('Le membre demandé n\'existe pas.');
+        }
+
         return $this->render('membre/show.html.twig', [
             'membre' => $membre
         ]);
     }
+
 }
